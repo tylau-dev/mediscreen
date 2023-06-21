716410cb6d802f608d0eafe33a3ee6a59fcc867b
Tsy-Yeung LAU <tsyyeung.lau@exakis-nelite.com>
2023-06-21 13:58:29 +0200

    rework front controller + repackage all dependencies + added initial sql

---
+++ query.sql
@@ -0,0 +1,15 @@
+USE mediscreen;
+
+CREATE TABLE patient (
+  patient_id tinyint(4) NOT NULL AUTO_INCREMENT,
+  first_name VARCHAR(125),
+  last_name VARCHAR(125),
+  birth_date TIMESTAMP,
+  gender VARCHAR(15),
+  address VARCHAR(125),
+  telephone VARCHAR(125),
+  PRIMARY KEY (patient_id)
+);
+
+INSERT INTO patient(first_name, last_name, birth_date, gender, address, telephone) values("John", "Doe", "2023-01-01", "M", "123 Rue de la Place 12345 Paris", "0123456789");
+INSERT INTO patient(first_name, last_name, birth_date, gender, address, telephone) values("Jeff", "Brian", "2023-01-01", "M", "122 Rue de la Place 12345 Paris", "0123456789");
